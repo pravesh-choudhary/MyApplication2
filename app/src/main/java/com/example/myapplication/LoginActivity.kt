2cd59b5f1a224d83eca59d77eb39bd691a9b6551
pravesh-choudhary <99267100+pravesh-choudhary@users.noreply.github.com>
2022-02-14 12:30:20 +0530

    Changed login ui to jetpack compose

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,8 +2,16 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.ActivityLoginBinding
@@ -16,13 +24,66 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding:ActivityLoginBinding=DataBindingUtil.setContentView(this,R.layout.activity_login)
         loginViewModel=ViewModelProvider(this).get(LoginViewModel::class.java)
-        binding.btnSubmit.setOnClickListener{
-            view->loginViewModel.onClick(view)
-            val email:String=binding.txtEmail.text.toString()
-            if(checkEmail(email)){
-                startActivity(Intent(this,HomeActivity::class.java))
-            }else{
-                binding.txtEmail.setError("Not Proper Email")
+
+        binding.composeView.setContent {
+            var mail = rememberSaveable { mutableStateOf("") }
+            var pass= rememberSaveable{ mutableStateOf("") }
+            var isError= rememberSaveable{
+                mutableStateOf(false)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    ,verticalArrangement = Arrangement.Center
+            ){
+                OutlinedTextField(value = mail.value,
+                    onValueChange = {
+                    mail.value=it
+                    isError.value=false },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start=20.dp,end=20.dp,top=20.dp),
+                    placeholder = {
+                        Text("Enter Email")
+                    },
+                    isError = isError.value,
+
+                    )
+                if(isError.value){
+                  Text(
+                      text = "Not a valid email address",
+                      color= Color.Red,
+                      modifier = Modifier
+                          .padding(start=20.dp,top=5.dp)
+
+                  )
+                }
+                OutlinedTextField(value = pass.value, onValueChange = {pass.value=it},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    placeholder = {
+                        Text("Enter Password")
+                    }
+                )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                    ,
+                    onClick = {
+                        if(checkEmail(mail.value)){
+                            startHome()
+                            loginViewModel.email.value=mail.value
+                            loginViewModel.pass.value=pass.value
+                        }else{
+                            isError.value=true
+                        }
+                     },
+                ){
+                    Text(
+                        text = "Submit"
+                    )
+                }
             }
         }
 
@@ -40,4 +101,8 @@ class LoginActivity : AppCompatActivity() {
     private fun checkEmail(email: String): Boolean {
         return EMAIL_ADDRESS_PATTERN.matcher(email).matches()
     }
+    private fun startHome(){
+        startActivity(Intent(applicationContext,HomeActivity::class.java))
+    }
 }
+
